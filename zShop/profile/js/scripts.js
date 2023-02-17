65620ef1252e811648b8ecac2175af105a9ec4ce
YukaChka <maxzerovol@gmail.com>
2023-02-17 14:02:25 +0300

    update

--- zShop/profile/js/scripts.js
+++ zShop/profile/js/scripts.js
@@ -1,17 +1,3 @@
-( function( $ ) {
-$( document ).ready(function() {
-$('#cssmenu').prepend('<div id="menu-button">Menu</div>');
-	$('#cssmenu #menu-button').on('click', function(){
-		var menu = $(this).next('ul');
-		if (menu.hasClass('open')) {
-			menu.removeClass('open');
-		}
-		else {
-			menu.addClass('open');
-		}
-	});
-});
-} )( jQuery );
 /*
    
     Template Name : DevBlog - Personal Blog Template
